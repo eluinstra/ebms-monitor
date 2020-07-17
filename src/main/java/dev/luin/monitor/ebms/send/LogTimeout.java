f2920590c03a6982cddf0d6ef886cc1164776ef7
eluinstra <eluinstra@luin.dev>
2020-07-17 22:13:12 +0200

    implemented simple ebms monitor

---
+++ src/main/java/dev/luin/monitor/ebms/send/LogTimeout.java
@@ -0,0 +1,16 @@
+package dev.luin.monitor.ebms.send;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class LogTimeout implements JavaDelegate
+{
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
